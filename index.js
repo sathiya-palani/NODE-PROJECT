ec32de9f82b3d197113d1617c5063966c1181bf0
Sathiya@1998 <sathiyavenkatachalam258@gmail.com>
2024-06-24 07:38:21 +0530

    db connectivity done

--- index.js
+++ index.js
@@ -1,47 +1,80 @@
+// 1. import express module
+const express = require('express');
+const mongoose = require('mongoose');
 
- const posts = [
+// 2. create express app
+const app = express();
+
+// 5. middleware
+app.use(express.json());
+
+// 4. create a route
+// app.get('/posts', (request, response) => {
+//     response.json(posts);
+// });
+
+// app.post('/posts', (request, response) => {
+//     posts.push(
+//         {
+//             ...request.body,
+//             id: posts.length + 1
+//         }
+//     );
+//     response.json(
+//         {
+//             ...request.body,
+//             id: posts.length
+//         }
+//     );
+// })
+
+// app.put('/posts/:id', (request, response) => {
+//     const id = request.params.id;
+//     let updatedPost = request.body;
+//     const toUpdatePost = posts.find(post => post.id == id);
+
+//     updatedPost = {
+//         ...toUpdatePost,
+//         ...updatedPost
+//     }
+
+//     posts = posts.map(post => post.id == id ? updatedPost : post);
+
+//     response.json(updatedPost);
+// })
+
+// app.delete('/posts/:id', (request, response) => {
+
+//     // find the post to delete
+//     const postToDelete = posts.find(post => post.id == request.params.id);
+
+//     if (!postToDelete) {
+//         response.send({
+//             message: 'Post not found'
+//         })
+//     }
+
+//     posts = posts.filter(post => post.id != request.params.id);
+//     response.json({
+//         message: 'Post deleted successfully'
+//     });
+// })
+
+// 3. run the server
+
+console.log(`Connecting to MongoDB...`);
+
+mongoose.connect(`mongodb+srv://sathiyavenkatachalam258:Sathiya123@cluster0.g32w2ad.mongodb.net/`)
+    .then(() => {
+        console.log('Connected to MongoDB');
+
+        // after connecting to MongoDB, start the server
+        app.listen(3001, () => {
+            console.log(`Server is running on http://localhost:3001`);
+        });
+    })
+    .catch((error) => {
+        console.log('Error connecting to MongoDB', error);
+    });
+
     
-        {
-            "userId": 1,
-            "id": 1,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-        },
-        {
-            "userId": 1,
-            "id": 2,
-            "title": "qui est esse",
-            "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-        },
-        {
-            "userId": 1,
-            "id": 3,
-            "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-            "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-        }
- ]
-
- //1. import express module
-const express = require ('express');
-
-// 2. Create a express application
- const app = express();
-
- //4. Create a route
- app.get ('/posts', (request, response) =>{
-    response.json(posts);
- });
-
- app.post('/posts' , (request ,response) =>{
-    response.json (
-        {
-            ...request.body,
-            id:posts.length + 1
-        }
-    ) 
- })
-
- //3. Run the server
- app.listen(3001,() => {
-    console.log('server is running on http://localhost:3001');
- });
